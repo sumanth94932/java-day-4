f8de71c1c60cc93d222288464ce74760d4d48281
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-05 16:03:31 +0530

    Add files via upload

---
+++ upda.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class upda{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        System.out.println(t++);
+        int a=s.nextInt();
+        System.out.println(++t);
+        int b=s.nextInt();
+        System.out.print(b--);
+        int c=s.nextInt();
+        System.out.println(--c);
+        }
+    }
