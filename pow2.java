93dd79f6bb93a95b8c793cd556d934a93a454a5f
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-05 12:41:25 +0530

    Add files via upload

---
+++ pow2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class pow2{
+    public static void  main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=1;i<t;i=i*2)
+        {
+            System.out.println(i*2);
+        }
+    }
+}
