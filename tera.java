297bb7e069af189a5129a6249da189f35c487d69
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-05 16:07:21 +0530

    Add files via upload

---
+++ tera.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class tera{
+    public static void  main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        System.out.println((a<b)?"a is smaller":"b is smaller");
+    }
+}
