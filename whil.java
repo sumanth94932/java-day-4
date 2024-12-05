c5fc3b70a48278b18f029cf69cd21d856d1fc5b6
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-05 16:24:48 +0530

    Add files via upload

---
+++ whil.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class whil{
+    public static void  main(String args[]){
+        int n=100;
+        int i=n;
+        while(i>-1)
+        {
+            System.out.println(i);
+            i-=1;
+        }
+    }
+}
+       
